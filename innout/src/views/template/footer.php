e400a9b20b7827667761fa1a46dc1760743566b7
Fernando Froehlich Soares <90044560+Fernando-Froehlich@users.noreply.github.com>
2022-01-21 15:09:57 -0300

    Aula 259 Barra Lateral Menu
    Atualização

---
+++ innout/src/views/template/footer.php
@@ -0,0 +1,7 @@
+<footer class="footer">
+    <span>Desenvolvido com</span>
+    <span><i class="icofont-heart text-danger mx-1"></i></span>
+    <span>por Ma Bietch</span>
+</footer>
+</body>
+</html>
