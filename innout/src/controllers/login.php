1ca6f2875218182f537630120ff332d62c150597
Fernando Froehlich Soares <90044560+Fernando-Froehlich@users.noreply.github.com>
2022-01-27 11:33:39 -0300

    Menu Usuário

--- innout/src/controllers/login.php
+++ innout/src/controllers/login.php
@@ -1,16 +1,17 @@
 <?php
 loadModel('Login');
+session_start();
 $exception = null;
- 
+
 if(count($_POST) > 0) {
     $login = new Login($_POST);
     try{
         $user = $login->checkLogin();
-        echo "Usuário {$user->name} logado :)";
+        $_SESSION['user'] = $user;
         header("Location: day_records.php");
     } catch(AppException $e) {
         $exception = $e;
     }
 }
- 
+
 loadView('login', $_POST + ['exception' => $exception]);
