25864c994f4b6354e599a591cd319276512bef74
Fernando Froehlich Soares <90044560+Fernando-Froehlich@users.noreply.github.com>
2022-01-29 10:26:49 -0300

    Populate

--- innout/src/controllers/data_generator.php
+++ innout/src/controllers/data_generator.php
@@ -61,7 +61,9 @@ function populateWorkingHours($userId, $initialDate, $regularRate, $extraRate, $
 
 $lasMonth = strtotime('first day of last month');
 
-// populateWorkingHours(1, date('Y-m-1'), 70, 20, 10);
+populateWorkingHours(1, date('Y-m-1'), 70, 20, 10);
+populateWorkingHours(3, date('Y-m-1'), 20, 75, 5);
+populateWorkingHours(4, date('Y-m-1'), 20, 10, 70);
 
 
 echo 'Tudo certo';
