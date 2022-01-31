714665633016ea911bb8cda8c0df5827ad0dbe76
Fernando Froehlich Soares <90044560+Fernando-Froehlich@users.noreply.github.com>
2022-01-31 12:02:43 -0300

    commit

--- innout/src/controllers/data_generator.php
+++ innout/src/controllers/data_generator.php
@@ -61,7 +61,7 @@ function populateWorkingHours($userId, $initialDate, $regularRate, $extraRate, $
 
 $lastMonth = strtotime('first day of last month');
 
-// populateWorkingHours(1, date('Y-m-1'), 70, 20, 10);
+populateWorkingHours(1, date('Y-m-1'), 70, 20, 10);
 // populateWorkingHours(3, date('Y-m-d', $lastMonth), 20, 75, 5);
 // populateWorkingHours(4, date('Y-m-d', $lastMonth), 20, 10, 70);
 
