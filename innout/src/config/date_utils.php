d9739ebbe3a583c3e8fab20a8eb784d0b0a5a9fd
Fernando Froehlich Soares <90044560+Fernando-Froehlich@users.noreply.github.com>
2022-01-31 11:40:17 -0300

    commit

--- innout/src/config/date_utils.php
+++ innout/src/config/date_utils.php
@@ -1,22 +1,22 @@
 <?php
 
-function getDateAsDatetime($date){
+function getDateAsDateTime($date){
     return is_string($date) ? new DateTime($date) : $date;
 }
 
 function isWeekend($date){
-    $inputDate = getDateAsDatetime($date);
+    $inputDate = getDateAsDateTime($date);
     return $inputDate->format('N') >= 6;
 }
 
 function isBefore($date1, $date2){
-    $inputDate1 = getDateAsDatetime($date1);
-    $inputDate2 = getDateAsDatetime($date2);
+    $inputDate1 = getDateAsDateTime($date1);
+    $inputDate2 = getDateAsDateTime($date2);
     return $inputDate1 <= $inputDate2;
 }
 
 function getNextDay($date){
-    $inputDate = getDateAsDatetime($date);
+    $inputDate = getDateAsDateTime($date);
     $inputDate->modify('+1 day');
     return $inputDate;
 }
--- innout/src/controllers/data_generator.php
+++ innout/src/controllers/data_generator.php
@@ -1,6 +1,6 @@
 <?php
 
-// loadModel('workingHours');
+loadModel('workingHours');
 
 Database::executeSQL('DELETE FROM working_hours');
 Database::executeSQL('DELETE FROM users WHERE id > 5');
@@ -41,31 +41,31 @@ function getDayTemplateByOdds($regularRate, $extraRate, $lazyRate){
 
 }
 
-// function populateWorkingHours($userId, $initialDate, $regularRate, $extraRate, $lazyRate){
-//     $currentDate = $initialDate;
-//     $today = new DateTime();
-//     $columns = ['user_id' => $userId, 'work_date' => $currentDate];
+function populateWorkingHours($userId, $initialDate, $regularRate, $extraRate, $lazyRate){
+    $currentDate = $initialDate;
+    $today = new DateTime();
+    $columns = ['user_id' => $userId, 'work_date' => $currentDate];
 
-//     while(isBefore($currentDate, $today)){
-//         if(!isWeekend($currentDate)){
-//             $template = getDayTemplateByOdds($regularRate, $extraRate, $lazyRate);
-//             $columns = array_merge($columns, $template);
-//             $workingHours = new WorkingHours($columns);
-//             $workingHours->insert();
-//         }
-//         $currentDate = getNextDay($currentDate)->format('Y-m-d');
-//         $columns['work_date'] = $currentDate;
-//     }
+    while(isBefore($currentDate, $today)){
+        if(!isWeekend($currentDate)){
+            $template = getDayTemplateByOdds($regularRate, $extraRate, $lazyRate);
+            $columns = array_merge($columns, $template);
+            $workingHours = new WorkingHours($columns);
+            $workingHours->save();
+        }
+        $currentDate = getNextDay($currentDate)->format('Y-m-d');
+        $columns['work_date'] = $currentDate;
+    }
 
-// }
+}
 
-// $lastMonth = strtotime('first day of last month');
+$lastMonth = strtotime('first day of last month');
 
 // populateWorkingHours(1, date('Y-m-1'), 70, 20, 10);
 // populateWorkingHours(3, date('Y-m-d', $lastMonth), 20, 75, 5);
 // populateWorkingHours(4, date('Y-m-d', $lastMonth), 20, 10, 70);
 
 
-// echo 'Tudo certo';
+echo 'Tudo certo<br>';
 
 print_r(getDayTemplateByOdds(10, 20, 70));
